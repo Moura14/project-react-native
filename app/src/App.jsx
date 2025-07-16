d187df414d276d37c0e8e8b3626d9b1063146110
João Gabriel <87152738+Moura14@users.noreply.github.com>
2025-07-16 16:13:17 -0300

    feat: componente cabeçalho

---
+++ app/src/App.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+import Header from './components/Header'
+
+
+export default class App extends Component {
+    render (){
+        return (
+            <Header></Header>
+        )
+    }
+}
---
+++ app/src/components/Header.jsx
@@ -0,0 +1,49 @@
+import { Component } from 'react'
+import { Image, Platform, StyleSheet, Text, View } from 'react-native'
+import icon from '../../../assets/images/icon.png'
+
+
+
+class Header extends Component {
+    render(){
+        return (
+            <View style={styles.container} 
+            >
+
+            <View style={styles.rowContainer}>
+                <Image source={icon} style={styles.image}></Image>
+                <Text style={styles.title}>Instagram</Text>
+
+            </View>
+
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: Platform.OS === 'ios' ? 20 : 40,
+        padding: 10, 
+        borderBottomWidth: 1,
+        borderColor: '#BBB'
+    },
+    rowContainer: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    image: {
+        height: 30,
+        width: 30,
+        resizeMode: 'contain'
+    },
+    title: {
+        color: '#000',
+        fontFamily: 'shelter',
+        height: 30,
+        fontSize: 28
+    }
+})
+
+export default Header
